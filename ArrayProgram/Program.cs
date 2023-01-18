4986b3d23013db1fa4ded55be0e33043a600792a
It Hours Solutions <It Hours Solutions@DESKTOP-LARMGGP>
2023-01-18 10:57:59 +0530

    Add project files.

---
+++ ArrayProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter Sise of array :");
+            int x=Convert.ToInt32(Console.ReadLine());
+            int[]arr=new int[x];
+            for(int y = 0; y < x; y++) 
+            {
+                Console.Write("Enter the " + y + " Element : ");
+                arr[y] = Convert.ToInt32(Console.ReadLine());
+            }
+            for(int z=0;z<arr.Length;z++) 
+            {
+                Console.Write(arr[z]+" ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
